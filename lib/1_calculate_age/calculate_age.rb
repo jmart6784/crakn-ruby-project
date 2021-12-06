b2a3d4b0432caca722fd75a8d2422a4f0b4906f6
jmart6784 <jmartinez6784@yahoo.com>
2021-12-05 17:44:29 -0800

    Implemented calculate age method

--- lib/1_calculate_age/calculate_age.rb
+++ lib/1_calculate_age/calculate_age.rb
@@ -1,5 +1,10 @@
+require 'date'
+
 class CalculateAge
   def self.calculate(age_at_birth, age_at_death)
-    # Write your code here
+    age_in_seconds = age_at_death.to_time.to_i - age_at_birth.to_time.to_i
+
+    time = Time.at(age_in_seconds)
+    time.year - 1970 + (time.yday / 365.25).floor
   end
 end
